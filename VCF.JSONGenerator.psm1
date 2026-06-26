0a458edc1e191ffdac2d32ba29b692bcdcf9aac9
Ken Gould <ken.gould@broadcom.com>
2026-06-26 10:56:00 +0100

    Update VCF.JSONGenerator.psm1
    .
    Signed-off-by: Ken Gould <ken.gould@broadcom.com>

--- VCF.JSONGenerator.psm1
+++ VCF.JSONGenerator.psm1
@@ -1203,7 +1203,6 @@ Function Get-NetworkDetailsFromGateway
     If ($parts.Count -ne 2)
     {
         LogMessage -type ERROR -message "Invalid input '$gatewayCidr'. Expected format: <gateway>/<prefix>, e.g. 192.168.1.1/24"
-        Return
     }
 
     $gwAddress  = $parts[0]
